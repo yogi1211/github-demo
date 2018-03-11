ef1e04bf329ee0287db5b92440074d5e10857b9d
yogi1211 <ilikeapple2@yahoo.com.vn>
2018-03-11 19:06:59 -0400

    Update app-component.ts
    Added import part

--- app-component.ts
+++ app-component.ts
@@ -1,7 +1,9 @@
+import {Component} from "@angular/core"
 @Component({
    selector:"app-component",
    template:"app-component.html",
    style:"app-component.css"
   })
 export class AppComponent{
+   
 }
